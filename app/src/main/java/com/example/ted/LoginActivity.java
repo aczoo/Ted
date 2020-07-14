c3ee4b05588d1f1c5e379b8b940787de941339fd
Annie Zhu <aczhu@fb.com>
2020-07-14 01:19:28 -0400

    firebase authentication with email

--- app/src/main/java/com/example/ted/LoginActivity.java
+++ app/src/main/java/com/example/ted/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.ted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
 
@@ -17,18 +18,31 @@ import android.widget.Toast;
 
 import com.example.ted.MainActivity;
 import com.example.ted.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     public static final String TAG = "LoginActivity";
-    public EditText etUsername;
-    public EditText etPassword;
+    public EditText etUsername, etPassword;
     public Button btnLogin;
     public CheckBox checkBox;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
         btnLogin = findViewById(R.id.btnLogin);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -49,20 +63,31 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void clicked(View view) {
         Log.i(TAG, "Clicked login");
-        String username = etUsername.getText().toString();
-        String password = etPassword.getText().toString();
-        loginUser(username, password);
+        mAuth.signInWithEmailAndPassword(etUsername.getText().toString(), etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    protected void updateUI(FirebaseUser user) {
+        if (user != null){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
-    protected void loginUser(String username, String password) {
-        Log.i(TAG, "Attempting to login " + username + " in with password " +password);
-        // CALL FIREBASE
-    }
-
-    private void goMainActivity() {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-        finish();
-
-    }
 }
--- app/src/main/java/com/example/ted/SignUpActivity.java
+++ app/src/main/java/com/example/ted/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.ted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,24 +8,44 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpActivity extends LoginActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         btnLogin.setText("Sign Up");
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Clicked sign up");
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                createUser(username, password);
-            }
-        });
     }
-    private void createUser(final String username, final String password){
-        Log.i(TAG, "Attempting to create account : "+ username);
-        //FIREBASE STUFF
+
+    @Override
+    public void clicked(View view) {
+        Log.i(TAG, "Clicked sign up");
+
+        mAuth.createUserWithEmailAndPassword(etUsername.getText().toString(),etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+
     }
 }
