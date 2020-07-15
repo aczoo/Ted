77f35cf74e830c3e2ac867f603dfbbe145ad97e9
Annie Zhu <aczhu@fb.com>
2020-07-15 13:09:58 -0400

    Format article body: Clickable span and new lines

--- app/src/main/java/com/example/ted/ArticleDetails.java
+++ app/src/main/java/com/example/ted/ArticleDetails.java
@@ -2,14 +2,29 @@ package com.example.ted;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.ted.models.Article;
 
 import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
@@ -21,16 +36,48 @@ public class ArticleDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_details);
+
         tvTitle = findViewById(R.id.tvTitle);
         tvAuthor = findViewById(R.id.tvAuthor);
         tvBody =findViewById(R.id.tvBody);
         ivThumbnail = findViewById(R.id.ivPicture);
+
         article = Parcels.unwrap(getIntent().getParcelableExtra(Article.class.getSimpleName()));
         tvTitle.setText(article.getTitle());
         tvAuthor.setText(article.getAuthor());
-        tvBody.setText(article.getBody());
+        fillLinks();
         String url = article.getImageUrl();
         Glide.with(ArticleDetails.this).load(url).transform(new RoundedCornersTransformation(15, 15)).placeholder(R.drawable.no_result).into(ivThumbnail);
 
     }
+    public void fillLinks(){
+        SpannableString ss = new SpannableString(article.getBody());
+       List<ClickableSpan> cs= new ArrayList<>();
+        final List<String> links= article.getLinks();
+        List<int[]> index = article.getIndex();
+        for(final String i:links) {
+            cs.add(new ClickableSpan() {
+                @Override
+                public void onClick(View widget) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(i));
+                    startActivity(browserIntent);
+                }
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setColor(getResources().getColor(R.color.blue));
+                    ds.setUnderlineText(false);
+                }
+            });
+        }
+
+        for( int i = 0;i<cs.size();i++)
+            ss.setSpan(cs.get(i),index.get(i)[0], index.get(i)[1], Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tvBody.setText(ss);
+        tvBody.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/ted/models/Article.java
+++ app/src/main/java/com/example/ted/models/Article.java
@@ -1,7 +1,15 @@
 package com.example.ted.models;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Movie;
+import android.net.Uri;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,28 +17,39 @@ import org.json.JSONObject;
 import org.parceler.Parcel;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+
 @Parcel
 public class Article {
     private final String TAG = "Article";
     String author, title, body, publishedAt, articleUrl, imageUrl;
+    List<int[]> index;
+    List<String> links;
+    StringBuilder sb;
 
-    public Article() {}
+    public Article() {
+    }
 
     public Article(JSONObject jsonObject) throws JSONException {
-        author =jsonObject.getJSONArray("tags").getJSONObject(0).getString("webTitle");
+        author = jsonObject.getJSONArray("tags").getJSONObject(0).getString("webTitle");
         title = jsonObject.getString("webTitle");
-        body =jsonObject.getJSONObject("fields").getString("body");
-        clean();
+        body = jsonObject.getJSONObject("fields").getString("body");
         publishedAt = jsonObject.getString("webPublicationDate");
         articleUrl = jsonObject.getString("webUrl");
         imageUrl = jsonObject.getJSONObject("fields").getString("thumbnail");
+        index = new ArrayList<>();
+        links = new ArrayList<>();
+        clean();
 
 
     }
+
     public static List<Article> fromJsonArray(JSONArray articleJson) throws JSONException {
-        List<Article> l= new ArrayList<>();
-        for(int i = 0; i<articleJson.length();i++){
+        List<Article> l = new ArrayList<>();
+        for (int i = 0; i < articleJson.length(); i++) {
             l.add(new Article(articleJson.getJSONObject(i)));
         }
         return l;
@@ -39,20 +58,50 @@ public class Article {
     public String getAuthor() {
         return author;
     }
-    public String getTitle(){
+
+    public String getTitle() {
         return title;
     }
-    public String getImageUrl(){
+
+    public String getImageUrl() {
         return imageUrl;
     }
-    public String getBody(){ return body;}
-    private void clean(){
-        Log.d(TAG, "clean: "+ title);
-        body=body.replaceAll("</p><p>","\n");
-        body=body.replaceAll("<p></p>","\n");
-        body=body.replaceAll("<p>","");
-        body=body.replaceAll("</p>","");
 
-
+    public List getLinks() {
+        return links;
     }
+
+    public List getIndex() {
+        return index;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    private void clean() {
+        Log.d(TAG, "clean: " + title);
+        body = body.replaceAll("<p>", "\n");
+        body = body.replaceAll("</p>", "\n");
+        sb = new StringBuilder(body);
+        int index = sb.indexOf("<a href=\"");
+        while (index >= 0) {
+            sb.delete(index, index + 9);
+            setLink(index);
+            index = sb.indexOf("<a href=\"");
+        }
+        body = sb.toString();
+    }
+
+    private void setLink(int i) {
+        int i2 = sb.indexOf("\">", i);
+        String link = sb.substring(i, i2);
+        sb.delete(i, i2 + 2);
+        i2 = sb.indexOf("</a>");
+        sb.delete(i2, i2 + 4);
+        links.add(link);
+        index.add(new int[]{i, i2});
+    }
+
+
 }
